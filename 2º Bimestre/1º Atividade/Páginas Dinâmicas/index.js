0deda0f60c12df12ff27ee536bc628ca1427b2b1
Vittor Nascimento Sirqueira <144181234+VittorN-01@users.noreply.github.com>
2024-04-29 20:08:23 -0300

    Add files via upload

---
+++ 2º Bimestre/1º Atividade/Páginas Dinâmicas/index.js
@@ -0,0 +1,41 @@
+const http = require("http")
+const url = require("url")
+const fs = require("fs")
+const porta = 5000
+
+http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/html" })
+    
+    const urlInfo = url.parse(req.url, true)
+    const nome = urlInfo.query.nome
+    const anoNasc = parseInt(urlInfo.query.anoNasc)
+    const anoAtual = parseInt(urlInfo.query.anoAtual)
+
+    let inicio = fs.readFileSync('inicio.html')
+    let fim = fs.readFileSync('fim.html')
+    let resposta = fs.readFileSync('resposta.html', 'utf8')
+
+    if (!nome || isNaN(anoNasc) || isNaN(anoAtual)) {
+        res.write(inicio)
+        res.write(fim)
+        return res.end()
+    } else {
+        const idade = anoAtual - anoNasc
+        const nameNewLine = nome + ", " + idade + " anos" + "\r\n"
+        fs.appendFile("arquivo.txt", nameNewLine, (err) => {
+            if (err) {
+                console.error("Erro ao atualizar o arquivo:", err)
+                res.writeHead(500)
+                return res.end()
+            }
+            console.log("Arquivo Atualizado")
+            
+            const resp = resposta.replace('{nome}', nome).replace('{idade}', idade)
+            res.write(resp)
+            res.end()
+        })
+    }
+
+}).listen(porta, () => {
+    console.log(`Servidor rodando na porta: ${porta}`)
+})
